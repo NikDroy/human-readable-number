1c4182dd49af99acaeabc0965eae7265fada5306
Nikta <gulyasnik@gmail.com>
2023-01-11 21:25:23 +0300

    update runs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 module.exports = function toReadable (number) {
     let firstTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
-    let tens = 'nine hundred eighty one';
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
     if(number < 20) {
         return firstTwenty[number]
-    } 
-    if (number === 981){
-        return tens
+    }
+    if (number > 20 && number < 100) {
+        let arrNum = number.toString().split('')
+        return tens[arrNum[0] - 2] + ' ' + firstTwenty[arrNum[1]]
     }
 }
+
+console.log(toReadable(99))
