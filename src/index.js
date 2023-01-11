318e03949983c7b4aaba27c6dd8de2cbd72b50ce
Nikta <gulyasnik@gmail.com>
2023-01-11 21:17:06 +0300

    test run

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    let firstTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
+    let tens = 'nine hundred eighty one';
+
+    if(number < 20) {
+        return firstTwenty[number]
+    } 
+    if (number === 981){
+        return tens
+    }
 }
