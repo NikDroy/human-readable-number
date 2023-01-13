7bc599b5532e5e28f700dfb6d965c697088ab44f
Nikta <gulyasnik@gmail.com>
2023-01-13 22:09:39 +0300

    test solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 module.exports = function toReadable (number) {
-    let firstTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
-    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let hundreds = []
+    let firstNums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let secondNums = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let arrNum = number.toString().split('');
-    if(number < 20) {
-        return firstTwenty[number]
+    let res = '';
+    if(number < 10) {
+        res = firstNums[arrNum];
     }
-    if (number => 20 && number < 100) {
-        let arrNum = number.toString().split('');
+    if(number >= 10 && number < 20){
+        res = secondNums[arrNum[1]]
+    }
+    if(number >= 20 && number < 100) {
         if(arrNum[1] === '0'){
-            return tens[arrNum[0] - 2]
-        } else {
-           return tens[arrNum[0] - 2] + ' ' + firstTwenty[arrNum[1]] 
+            res = tens[arrNum[0]]
+        }else if(arrNum[1] !== '0'){
+           res = `${tens[arrNum[0]]} ${firstNums[arrNum[1]]}` 
         }
-        
     }
+    if(number >= 100) {
+        if(arrNum[1] === '1' && arrNum[2] === '0'){
+            res = `${firstNums[arrNum[0]]} hundred ten`; 
+        } 
+            else if(arrNum[1] === '0' && arrNum[2] !== '0'){
+                res = `${firstNums[arrNum[0]]} hundred ${firstNums[arrNum[2]]}`;
+            } 
+            else if(arrNum[2] === '0' && arrNum[1] !== '0'){
+                res = `${firstNums[arrNum[0]]} hundred ${tens[arrNum[1]]}`;
+            } 
+            else if(arrNum[1] === '1'){
+                res = `${firstNums[arrNum[0]]} hundred ${secondNums[arrNum[2]]}`;
+            }     
+            else if(number > 100 && number % 100 === 0){
+            res = `${firstNums[arrNum[0]]} hundred`;
+            }
+        else { 
+                res = `${firstNums[arrNum[0]]} hundred ${tens[arrNum[1]]} ${firstNums[arrNum[2]]}`;
+            }
+        }
+    return res;
 }
-
-// console.log(toReadable(25))
